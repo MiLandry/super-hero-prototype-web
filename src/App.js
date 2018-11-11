389ac07fda140418dfcc002186d448a9f2a1f1d4
Michael Landry <mlandryviolin@gmail.com>
2018-11-11 12:02:48 -0500

    add component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import DiceBag from './components/DiceBag'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <button>clickme</button>
-          
+        <DiceBag/>
+          <button>choose hero</button>
+          <button>change dice bag</button>
+          <button>create dice pool</button>
+          <div className="dice-pool">wwwr</div>
         </header>
       </div>
     );
---
+++ src/components/DiceBag.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './DiceBag.css';
+
+class DiceBag extends Component {
+  render() {
+    return (
+      <div className="dice-bag">
+      white die
+      </div>
+    );
+  }
+}
+
+export default DiceBag;
